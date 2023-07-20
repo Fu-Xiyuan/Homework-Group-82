82a6cde6c76952c8f81ad15b04ed1a8ca3c2f22e
Fu-Xiyuan <139515830+Fu-Xiyuan@users.noreply.github.com>
2023-07-20 19:55:11 +0800

    SM3-Birthday-Attack.py

--- Project-1/SM3-Birthday-Attack.py
+++ Project-1/SM3-Birthday-Attack.py
@@ -13,13 +13,13 @@ def birthday_attack():
             # 随机生成消息
             message = secrets.token_hex(16)
             # 计算对应的hash值
-            same_value = SM3.Hash_sm3(message)[:attack_size[i]]
+            same_value = SM3.sm3(message)[:attack_size[i]]
             # 如果该hash值存在且对应消息值不同，那么碰撞找到，结束
             if same_value in hash_table and message != hash_table[same_value]:
                 end_time = time.time()
                 use_time = end_time - stat_time
-                print("Message 1:", message, "Hash Value :", SM3.Hash_sm3(message))
-                print("Message 2:", hash_table[same_value], "Hash Value :", SM3.Hash_sm3(hash_table[same_value]))
+                print("Message 1:", message, "Hash Value :", SM3.sm3(message))
+                print("Message 2:", hash_table[same_value], "Hash Value :", SM3.sm3(hash_table[same_value]))
                 print("Same Value :", same_value)
                 print("Same Value Size :", attack_size[i]*4, "bits")
                 print("Time Use :", use_time)
